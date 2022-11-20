40c9484fc8f77bf4fd0ad6993b26488c5a61e341
dyrc9 <sldx000@sjtu.edu.cn>
2022-11-20 12:42:14 +0800

    gui initial

---
+++ gui.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root=Tk()
+root.geometry("400x400+200+200")
+label=Label(root, background="black", text="chat with your mom", font=("Time New Roman",20), fg="green", bg="pink")
+
+label.pack()
+
+
+root.mainloop()

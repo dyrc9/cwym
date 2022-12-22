19fd77cdfbf676e5e7cc30fc4fa15673d5dd0a6f
dyrc9 <sldx000@sjtu.edu.cn>
2022-12-22 14:38:39 +0800

    complete the server code

--- TCPserver.py
+++ TCPserver.py
@@ -1,4 +1,6 @@
 from socket import *
+import time
+from threading import Thread
 
 class sever:
     def __init__(self, severName = ' ', serverPort = 8088) -> None:
@@ -13,6 +15,7 @@ class sever:
         self.cAccount = {} #each client has a name, Dictionary.
         self.namelist = {'Alice', 'Bob', 'Carol', 'Dave', 'Eve'}
 
+    #connect to the client
     def connect(self):
         while True:
             client, address = self.severSocket.accept()
@@ -22,20 +25,33 @@ class sever:
             client.send(successmsg.encode())
             self.clients.append(client)
             self.cAccount[client] = self.namelist[self.clientNumber] #give the client a name
+            #when a client gets connected, start a thread to receive message
+            Thread(target=self.receive, args=(client)).start()
 
+    #receive message from a client
     def receive(self, client):
         while True:
             try:
                 msg = client.recv(1024).decode()
             except:
-                print("server receive get something wrong.")
+                print("server receive() get something wrong.")
                 exit()
+            if msg.upper() == 'Q':
+                self.closecon(client)
+                break
             self.forward(msg, client)    
 
+    #forward the message to all the client
     def forward(self, msg, client):
         for c in self.clients:
             if (client != c): 
-                c.send(((self.cAccount)+msg).encode())
+                c.send(((self.cAccount)+time.strftime("%x")+"\n"+msg).encode())
 
+    #close the connection with a client
+    def closecon(self, client):
+        self.clients.remove(client)
+        client.close
+        msg = self.cAccount[client]+"has left the chatroom"
+        self.forward(msg, client)
         
 
